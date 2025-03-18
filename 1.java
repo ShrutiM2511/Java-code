25a2f58ac86cffe56ad03b95900aa7b1d2b67964
ShrutiM2511 <mricha158@gmail.com>
2025-03-18 13:18:36 +0530

    my first code

---
+++ 1.java
@@ -0,0 +1,7 @@
+class msgprint
+   {
+       public static void main(String args[])
+        {
+            System.out.println("I am Shruti");
+        }
+   }
---
+++ 2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class united
+    {
+      public static void main(String args[]){
+           Scanner sc=new Scanner(System.in);
+           int n=sc.nextInt();
+           if(n%2==0){
+             System.out.println("Even Number");
+
+             }
+             else{
+               System.out.println("Odd Number");
+
+                 }
+
+        }
+      
+    }
