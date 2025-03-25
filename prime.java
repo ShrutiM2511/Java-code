049975b5bcc86e347e950d3509a132a2fd1f7547
ShrutiM2511 <mricha158@gmail.com>
2025-03-25 12:37:37 +0530

    prime number

---
+++ prime.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main
+{
+     public static boolean prime(int num){
+	        for(int i=2;i<num;i++){
+	            if(num%i==0)
+	            return false;
+	        }
+	        return true;
+	    }
+	    
+	   public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    boolean ans=prime(n);
+	    
+	    if(ans==true){
+	        System.out.println("prime");
+	    }else{
+	        System.out.println(" not prime");
+	    }
+	}
+}
